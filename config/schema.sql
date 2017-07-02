00d3e98ce6a3eca8d4a9779db8e9412fc8d2b581
Co de Waal <codewaal@home.nl>
2017-07-02 14:22:18 +0200

    Update schema.sql
    Extend buzzer gpio selection

--- config/schema.sql
+++ config/schema.sql
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS step
     id INTEGER PRIMARY KEY NOT NULL,
     "order" INTEGER,
     name VARCHAR(80),
-    type VARCHAR(100),
+1    type VARCHAR(100),
     stepstate VARCHAR(255),
     state VARCHAR(1),
     start INTEGER,
@@ -28,7 +28,7 @@ CREATE TABLE IF NOT EXISTS step
 );
 
 CREATE TABLE IF NOT EXISTS sensor
-(
+2(
     id INTEGER PRIMARY KEY NOT NULL,
     type VARCHAR(100),
     name VARCHAR(80),
@@ -38,7 +38,7 @@ CREATE TABLE IF NOT EXISTS sensor
 
 CREATE TABLE IF NOT EXISTS fermenter_step
 (
-    id INTEGER PRIMARY KEY NOT NULL,
+,    id INTEGER PRIMARY KEY NOT NULL,
     name VARCHAR(80),
     hours INTEGER,
     minutes INTEGER,
@@ -84,7 +84,7 @@ INSERT OR IGNORE INTO config VALUES ('actor_cols', 4, 'select', 'Adjust the widt
 INSERT OR IGNORE INTO config VALUES ('sensor_cols', 4, 'select', 'Adjust the width of a sensor widget on the brewing dashboard', '[1,2,3, 4, 5, 6, 7, 8, 9, 10, 11, 12]');
 INSERT OR IGNORE INTO config VALUES ('unit', 'C', 'select', 'Temperature Unit', '["C","F"]');
 INSERT OR IGNORE INTO config VALUES ('brewery_name', 'My Home Brewery', 'text', 'Your brewery name', NULL );
-INSERT OR IGNORE INTO config VALUES ('buzzer', 16, 'select', 'Buzzer GPIO', '[16,17,18,19,20]');
+INSERT OR IGNORE INTO config VALUES ('buzzer', 16, 'select', 'Buzzer GPIO', '[16,17,18,19,20,21,22,23,24,25,26]');
 INSERT OR IGNORE INTO config VALUES ('setup', 'YES', 'select', 'Show the Setup dialog', '["YES","NO"]');
 INSERT OR IGNORE INTO config VALUES ('brew_name', '', 'text', 'Brew Name', NULL);
 INSERT OR IGNORE INTO config VALUES ('donation_notification', 'YES', 'select', 'Disable Donation Notification', '["YES","NO"]');
